2a42ac97b7dc31d663b3c93441f2878bebabc6d2
YogurtTheHorse <yegorf1@gmail.com>
2019-02-25 03:00:03 +0300

    Imroove queue_with_priorities

--- stepik/queue_with_priorities.py
+++ stepik/queue_with_priorities.py
@@ -1,9 +1,1 @@
-# BTW: i didn't use orders with priorities to solve this problem
-from bisect import insort_left
-
-n = int(input())
-l = list()
-
-for i in range(n):
-    command, v, *_ = input().split() + ['0']
-    {'Insert': lambda: insort_left(l, int(v)), 'ExtractMax': lambda: print(l.pop())}[command]()
+a = [(lambda: [[(lambda command, v, *_: {'Insert': lambda: bisect.insort_left(l, int(v)), 'ExtractMax': lambda: print(l.pop())}[command]())(*(input().split() + ['0'])) for i in range(int(input()))]for l in [list()]])() for l in [[]] for bisect in [__import__('bisect')]]
